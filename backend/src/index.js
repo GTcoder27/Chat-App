cbef5082ed5eba9408a8674c8f5a1e0ff7b46dcf
GTcoder27 <girishnandanwar2708@gmail.com>
2025-04-22 22:45:47 +0530

    voice features added using bhashini api

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,17 +2,17 @@ import express from 'express';
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
-import {connectDB} from "./lib/db.js"
+import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import {server,app} from "./lib/socket.js"
+import { server, app } from "./lib/socket.js"
 import bodyParser from "body-parser";
 import path from "path";
 
 
 app.use(express.json({ limit: '50mb' }));
-app.use(bodyParser.json({ limit: '50mb' })); 
-app.use(bodyParser.urlencoded({ extended: true, parameterLimit:1000000 ,limit: '500mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, parameterLimit: 1000000, limit: '500mb' }));
 app.use('/api/messages/asr/voice_to_text', express.raw({ type: 'audio/*', limit: '50mb' }));
 app.use('/api/messages/tts/text_to_voice', express.raw({ limit: '50mb' }));
 
@@ -24,33 +24,33 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-    app.use(cors({
-        origin: process.env.FRONTEND_URL,
-        credentials: true,
-        // allowedHeaders: ["Content-Type", "Authorization"]  //Add other headers you want to pass through CORS request
-    }));
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    // allowedHeaders: ["Content-Type", "Authorization"]  //Add other headers you want to pass through CORS request
+}));
 // app.use(cors());
 
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production"){
-    console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+    console.log("Running in production mode");
+
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
     });
 }
 
 
-server.listen(PORT,()=>{
-    console.log('server is running on port '+ PORT);
+server.listen(PORT, () => {
+    console.log('server is running on port ' + PORT);
     connectDB();
 });
 
- 
+
 
 
 
